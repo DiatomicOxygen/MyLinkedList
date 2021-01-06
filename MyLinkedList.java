7d8400ada85294c06fec68d2c9b85b1ec40db1e4
DiatomicOxygen <>
2021-01-06 13:30:44 -0500

    add method works

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -10,5 +10,21 @@ public class MyLinkedList {
   public int size() {
     return size;
   }
-  
+
+  public boolean add(String value) {
+    if (size == 0) {
+      Node n = new Node(value);
+      start = n;
+      end = n;
+      size++;
+      return true;
+    } else {
+      Node n = new Node(value);
+      n.setPrev(end);
+      end.setNext(n);
+      end = n;
+      size++;
+      return true;
+    }
+  }
 }
